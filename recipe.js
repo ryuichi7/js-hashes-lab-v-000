29bbe140fca7343e1d38b814e58b3ef2d4863ab9
Brett <brettrheenan@gmail.com>
2016-03-18 16:51:06 +0100

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function addIngredient(recipe,ingredient,quantity) {
+	recipe[ingredient] = quantity;
+	return recipe;
+};
+
+function removeIngredient(recipe, ingredient) {
+	delete recipe[ingredient];
+	return recipe;
+};
+
+function updateIngredient(recipe,ingredient,quantity) {
+	recipe[ingredient] = quantity;
+	return recipe;
+};
+
+function readRecipe(recipe) {
+	if (Object.keys(recipe).length === 0) {
+		console.log("This recipe has no ingredients!");
+	} else {
+		for (var key in recipe) {
+			console.log("this recipe calls for " + recipe[key] + " of " + key);
+		};
+	};
+};
